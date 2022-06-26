6afdf9d19ea7c96603149b16264a48c5df49984a
chenhui0923 <251387518@qq.com>
2022-06-26 16:42:26 +0800

    1

--- src/components/first.vue
+++ src/components/first.vue
@@ -4,16 +4,26 @@
 		<span>{{ msg }}</span>
 		<p>{{now}}</p>
 		<div class="static" :class="{ active: isActive, 'text-danger': hasError }">1</div>
-		
+
 		<template v-if="loginType === 'username'">
-		  <label>Username</label>
-		  <input placeholder="Enter your username" key="username-input">
+			<label>Username</label>
+			<input placeholder="Enter your username" key="username-input">
 		</template>
 		<template v-else>
-		  <label>Email</label>
-		  <input placeholder="Enter your email address" key="email-input">
+			<label>Email</label>
+			<input placeholder="Enter your email address" key="email-input">
 		</template>
 		<button @click="btn1">切换</button>
+		<div v-for="(value, name, index) in object">
+			{{ index }}. {{ name }}: {{ value }}
+		</div>
+
+		<!--计算属性适用  -->
+		<li v-for="n in evenNumbers">{{ n }}</li>
+		<!-- 计算属性不适用 -->
+		<ul v-for="set in sets">
+			<li v-for="n in even(set)">{{ n }}</li>
+		</ul>
 	</div>
 </template>
 
@@ -25,29 +35,46 @@
 				message: 'Hello',
 				isActive: true,
 				hasError: true,
-				loginType:''
+				loginType: '',
+				object: {
+					title: 'How to do lists in Vue',
+					author: 'Jane Doe',
+					publishedAt: '2016-04-10'
+				},
+				numbers: [1, 2, 3, 4, 5],
+				sets: [[ 1, 2, 3, 4, 5 ], [6, 7, 8, 9, 10]]
 			}
 		},
 		methods: {
-			
+
 			Refresh() {
 				window.location.reload();
 
 			},
-		btn1(){
-			console.log(this.loginType);
-			if(this.loginType==''){
-				this.loginType= 'username';
-			}else{
-				this.loginType = '';
-			}
-			
-		}
-			
+			btn1() {
+				console.log(this.loginType);
+				if (this.loginType == '') {
+					this.loginType = 'username';
+				} else {
+					this.loginType = '';
+				}
+
+			},
+			even: function (numbers) {
+			    return numbers.filter(function (number) {
+			      return number % 2 === 0
+			    })
+			  }
+
 		},
 		computed: {
 			now: function() {
 				return Date.now()
+			},
+			evenNumbers: function() {
+				return this.numbers.filter(function(number) {
+					return number % 2 === 0
+				})
 			}
 		}
 	}
